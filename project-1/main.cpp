5f5b466c5529a37a6d4913188b772c3f114f3cbf
Andrew Gergis <mqange@linux-desktop13.tuni.fi>
2023-04-13 04:57:24 +0300

    updated the world file

--- project-1/main.cpp
+++ project-1/main.cpp
@@ -21,49 +21,46 @@ TUNI_WARN_ON()
 
 #include <thread>
 #include <chrono>
-#include <barrier>
-
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+  QApplication app(argc, argv);
 
-    // data model of the world
-    world::init();
+  // data model of the world
+  world::init();
 
-    // Qt pixmap of the world
-    graphics::init();
+  // Qt pixmap of the world
+  graphics::init();
 
-    // Qt graphics scene to show the pixmap
-    QGraphicsScene scene;
-    QGraphicsPixmapItem grarea(*(graphics::current_pixmap));
-    scene.addItem( &grarea );
+  // Qt graphics scene to show the pixmap
+  QGraphicsScene scene;
+  QGraphicsPixmapItem grarea(*(graphics::current_pixmap));
+  scene.addItem( &grarea );
 
-    // Qt graphics view of the pixmap world (scaled)
-    QGraphicsView grview(&scene);
-    grview.setCacheMode( QGraphicsView::CacheModeFlag::CacheNone );
-    grview.setTransformationAnchor(QGraphicsView::AnchorViewCenter);
-    grview.scale(config::scale, config::scale);
-
-
-    // timer to periodically update the graphics
-    GrTimer myTimer( &grarea );
-    QObject::connect(&app,SIGNAL(aboutToQuit()),&myTimer,SLOT(closing()));
+  // Qt graphics view of the pixmap world (scaled)
+  QGraphicsView grview(&scene);
+  grview.setCacheMode( QGraphicsView::CacheModeFlag::CacheNone );
+  grview.setTransformationAnchor(QGraphicsView::AnchorViewCenter);
+  grview.scale(config::scale, config::scale);
 
 
-    // new thread to periodically update the
-    // simulation world
+  // timer to periodically update the graphics
+  GrTimer myTimer( &grarea );
+  QObject::connect(&app,SIGNAL(aboutToQuit()),&myTimer,SLOT(closing()));
 
-    std::thread update( [&]() {
 
-        while(world::running) {
+  //    // new thread to periodically update the
+  //    // simulation world
+      std::thread update( [&]() {
+          while(world::running) {
+            std::this_thread::sleep_for( config::world_tick );
+            world::next_generation();
+          };
+      });
 
-          std::this_thread::sleep_for( config::world_tick );
-          world::next_generation();
-        };
-    });
-    grview.show();
-    app.exec(); // main thread handles the Qt event loop
-    update.join(); // collect update thread after app closes
-    return EXIT_SUCCESS;
+
+  grview.show();
+  app.exec(); // main thread handles the Qt event loop
+
+  return EXIT_SUCCESS;
 }
--- project-1/world.cpp
+++ project-1/world.cpp
@@ -10,56 +10,55 @@ TUNI_WARN_ON()
 #include <random>
 #include <iostream>
 #include <string>
-
-#include <vector>
-#include <thread>
 #include <mutex>
+#include <thread>
+#include <condition_variable>
+#include <barrier>
 
+#define THREADS 4
 
 namespace world {
 
-// actual variables (extern in header file):
-std::unique_ptr< world_t > current;
-std::unique_ptr< world_t > next;
+  // actual variables (extern in header file):
+  std::unique_ptr< world_t > current;
+  std::unique_ptr< world_t > next;
 
+  bool running = true;
+  std::mutex running_mutex;  // Mutex to protect the 'running' variable
 
-
-bool running = true;
-std::mutex running_mutex;
-
-namespace {
+  namespace {
     // QtCreator might give non-pod warning here, explanation:
     // https://github.com/KDE/clazy/blob/master/docs/checks/README-non-pod-global-static.md
     auto rand_generator = std::bind(std::uniform_int_distribution<>(0,1),std::default_random_engine());
 
     // glider data is from: https://raw.githubusercontent.com/EsriCanada/gosper_glider_gun/master/gosper.txt
     std::string glider_gun[] = {
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 1 0 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 1 0 0 1 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 1 0 0 0 1 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 1 0 0 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 1 1 1 1 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-        "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 1 0 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 1 0 0 1 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 1 0 0 0 1 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 1 0 0 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 1 1 1 1 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+      "0000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
     };
-}
+  }
 
-void init(void)
-{
+  void init(void)
+  {
     // creaate and fill empty world data
     current = std::make_unique<world_t>();
     current->fill(Block::empty);
@@ -70,21 +69,21 @@ void init(void)
     config::coord_t x = 0;
     config::coord_t y = 0;
     for( x = 0; x < config::width; ++x )
-    {
+      {
         for( y = 0; y < config::height; ++y )
-        {
+          {
             if( x < (config::width/2) and y < (config::height/2) )
-                continue; // leave space for the glider gun
+              continue; // leave space for the glider gun
 
             bool b = rand_generator();
             if(b) {
                 (*current)[xy2array(x,y)] = Block::empty;
-            } else {
+              } else {
                 (*current)[xy2array(x,y)] = Block::occupied;
-            }
+              }
 
-        }
-    }
+          }
+      }
 
 
     // glider gun data to world data
@@ -92,112 +91,88 @@ void init(void)
     x = glider_pos;
     y = glider_pos;
     for(auto& line : glider_gun)
-    {
+      {
         for(char c : line)
-        {
+          {
             if(c == '1') {
                 (*current)[xy2array(x,y)] = Block::occupied;
                 ++x;
-            } else if(c == '0') {
+              } else if(c == '0') {
                 (*current)[xy2array(x,y)] = Block::empty;
                 ++x;
-            } else {
+              } else {
                 // all other chars ignored
-            }
-        }
+              }
+          }
         x = glider_pos;
         ++y;
-    }
-}
+      }
+  }
 
-
-void next_generation_partition(int start_x, int end_x) {
-    for (config::coord_t x = start_x; (int) x < (int) end_x; x++) {
-        for (config::coord_t y = 0; y < config::height; y++) {
-
-            // to make sure that multiple threads do not access
-            // the variable at the same time
-            std::lock_guard<std::mutex> lock(running_mutex);
-             // nothing done if simulation is not running
-            if (world::running == false) return;
-
-            auto current_pos = xy2array(x, y);
-            auto neighbours = num_neighbours(x, y);
-
-             // https://en.wikipedia.org/wiki/Conway's_Game_of_Life#Rules
-            if ((*current)[current_pos] == Block::occupied && (neighbours == 2 || neighbours == 3)) {
-                // 1. stay alive if 2 or 3 neighbours
-                (*next)[current_pos] = Block::occupied;
-
-              }else if ((*current)[current_pos] == Block::empty && neighbours == 3) {
-                // 2. spawn new if exactly three neighbours
-                (*next)[current_pos] = Block::occupied;
-            }
-            else {
-                 // 3. else cell will be empty
-                (*next)[current_pos] = Block::empty;
-            }
-        }
-    }
-}
-
-void next_generation() {
-
-    // Create an instance of a stopwatch object to
-    // measure the time taken to compute the next generation
+  void next_generation()
+  {
     stopwatch clock;
 
-    // Create a vector of thread objects to execute the
-    // next_generation_part function in parallel
-    std::vector<std::thread> threads;
+    // Get the number of rows and columns in the world
+    config::coord_t rows = config::height;
+    config::coord_t cols = config::width;
 
-    // Define the number of threads to be used
-    const int num_threads = 12;
+    // Create a barrier to synchronize the threads after each generation
+    std::barrier barrier(THREADS);
 
-    // Calculate the chunk size for each thread to compute
-    const int chunk_size = config::width / num_threads;
+    // Create an array of threads to process the world in parallel
+    std::thread threads[THREADS];
 
-    // Calculate the leftover amount of cells to compute
-    // which will be added to the last chunk
-    const int leftover = config::width % num_threads;
+    // Calculate the number of rows to process in each thread
+    config::coord_t rows_per_thread = rows / THREADS;
 
-    // Initialize the starting x-coordinate
-    // for the first chunk of cells to compute.
-    int start_x = 0;
-    int end_x   = 0;
-    for (int i = 0; i < num_threads; i++) {
+    // Partition the data processing into multiple parts and run them in separate threads
+    for (int i = 0; i < THREADS; ++i) {
+        config::coord_t start_row = i * rows_per_thread;
+        config::coord_t end_row = (i == THREADS - 1) ? rows : start_row + rows_per_thread;
+        threads[i] = std::thread([&](config::coord_t start, config::coord_t end) {
 
-        // Calculate the ending x-coordinate
-        // for the current chunk of cells to compute.
-        end_x = start_x + chunk_size;
+            for (config::coord_t row = start; row < end; ++row) {
+          for (config::coord_t col = 0; col < cols; ++col) {
 
-        // If this is the last thread, add
-        // leftover amount of cells to compute to its chunk.
-        if (i == num_threads - 1) {
-            end_x += leftover;
+              if( world::running == false)
+                {
+                  std::unique_lock<std::mutex> lock(world::running_mutex);
+                  return;  // nothing done if simulation is not running
+                  lock.unlock();
+                }
+              // Perform the necessary data processing for each cell in the world
+              auto current_pos = xy2array(col, row);
+              auto neighbours = num_neighbours(col, row);
+
+
+              // https://en.wikipedia.org/wiki/Conway's_Game_of_Life#Rules
+              if ((*current)[current_pos] == Block::occupied && (neighbours == 2 || neighbours == 3)) {
+
+                  // 1. stay alive if 2 or 3 neighbours
+                  (*next)[current_pos] = Block::occupied;
+                } else if ((*current)[current_pos] == Block::empty && neighbours == 3) {
+                  // 2. spawn new if exactly three neighbours
+                  (*next)[current_pos] = Block::occupied;
+                } else {
+                  // 3. else cell will be empty
+                  (*next)[current_pos] = Block::empty;
+                }
+            }
         }
+        // Wait at the barrier to synchronize the threads
+        barrier.arrive_and_wait();
+      }, start_row, end_row);
+  }
 
-        // Create a new thread object
-        // and add it to the vector of threads
-        threads.emplace_back(next_generation_partition, start_x, end_x);
-
-        // Update the starting x-coordinate
-        // for the next chunk of cells to compute.
-        start_x = end_x;
+  // Join the threads to wait for them to finish
+  for (int i = 0; i < THREADS; ++i) {
+      threads[i].join();
     }
 
-    // Wait for all the threads to finish executing before proceeding.
-    for (auto& thread : threads) {
-        thread.join();
-    }
+  std::cerr << "next world created in: " << clock.elapsed() << std::endl;
 
-    // Print the time taken to compute the next generation to the standard error stream.
-    std::cerr << "next world created in: " << clock.elapsed() << std::endl;
-
-
-    std::lock_guard<std::mutex> lock(running_mutex);
-    // Swap the current and next world states to update the world to the next generation.
-    std::swap(current, next);
+  std::swap(current, next);
 }
 
 
